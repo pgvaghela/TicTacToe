cf9fd0ad4ce7e1c4d2dcafa6dbf423882fbfb219
pgvaghela <pgvaghela20@gmail.com>
2025-06-24 23:57:44 -0600

    Day 2: Add GameState logic (makeMove, checkWin, reset) and unit tests

--- TicTacToeTests/TicTacToeTests.swift
+++ TicTacToeTests/TicTacToeTests.swift
@@ -1,16 +1,36 @@
-//
-//  TicTacToeTests.swift
-//  TicTacToeTests
-//
-//  Created by Priyansh Vaghela on 6/22/25.
-//
+import XCTest
+@testable import TicTacToe
 
-import Testing
-
-struct TicTacToeTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+final class TicTacToeTests: XCTestCase {
+    /// X in the top row should be detected as a win.
+    @MainActor func testWinCondition() {
+        let gs = GameState()
+        gs.board.cells = [.x, .x, .x,
+                          nil, nil, nil,
+                          nil, nil, nil]
+        gs.checkWin()
+        XCTAssertTrue(gs.gameOver)
+        XCTAssertEqual(gs.winner, .x)
     }
 
+    /// A full board with no three‐in‐a‐row is a draw.
+    @MainActor func testDrawCondition() {
+        let gs = GameState()
+        gs.board.cells = [
+          .x, .o, .x,
+          .x, .o, .o,
+          .o, .x, .x
+        ]
+        gs.checkWin()
+        XCTAssertTrue(gs.gameOver)
+        XCTAssertNil(gs.winner)
+    }
+
+    /// After one move, the currentPlayer should switch.
+    @MainActor func testTurnSwitch() {
+        let gs = GameState()
+        gs.makeMove(at: 0)
+        XCTAssertEqual(gs.board.cells[0], .x)
+        XCTAssertEqual(gs.currentPlayer, .o)
+    }
 }
